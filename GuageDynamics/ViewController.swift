aa23b3dc2dc23848dca064cbbdb999ef109c3088
SRI KALYAN GANJA <srikalyan@ganjas-macbook-pro.local>
2017-09-17 22:10:06 -0500

    Initial Commit

---
+++ GuageDynamics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuageDynamics
+//
+//  Created by SRI KALYAN GANJA on 9/17/17.
+//  Copyright © 2017 SRI KALYAN GANJA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
